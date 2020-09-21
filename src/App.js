c97bc09a8278e9b1a586083daa34bb3dd54ece61
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-21 16:45:11 -0600

    fix App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>React FrontEnd</h1>
-    </div>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <div className="container">
+        <h1>React FrontEnd</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
