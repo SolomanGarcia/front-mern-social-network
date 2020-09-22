1ef82d43f391d1844ec7f10eed5452f7014d8eb2
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-22 10:08:40 -0600

    add username to navbar when signed in

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -63,19 +63,27 @@ const Menu = ({ history }) => (
       )}
 
       {isAuthenticated() && (
-        <li className="nav-item">
-          <a
-            href
-            className="nav-link"
-            style={
-              (isActive(history, "/signup"),
-              { cursor: "pointer", color: "#fff" })
-            }
-            onClick={() => signout(() => history.push("/"))}
-          >
-            Sign Out
-          </a>
-        </li>
+        <>
+          <li className="nav-item">
+            <a
+              href
+              className="nav-link"
+              style={
+                (isActive(history, "/signup"),
+                { cursor: "pointer", color: "#fff" })
+              }
+              onClick={() => signout(() => history.push("/"))}
+            >
+              Sign Out
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <a href className="nav-link">
+              {isAuthenticated().user.name}
+            </a>
+          </li>
+        </>
       )}
     </ul>
   </div>
