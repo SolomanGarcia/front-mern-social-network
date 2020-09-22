e1728eb2d5edb307b3c7fa4c3b694e93171a03e2
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-22 07:48:12 -0600

    create Signin component

---
+++ src/user/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Sign In</h2>
+      </div>
+    );
+  }
+}
+
+export default Signin;
