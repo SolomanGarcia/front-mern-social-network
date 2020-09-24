47d18663cbfaa0d9a2d025125ac5144bff337ce8
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-24 17:25:17 -0600

    add follow

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -12,7 +12,8 @@ class Profile extends Component {
     this.state = {
       user: { following: [], followers: [] },
       redirectToSignin: false,
-      following: false
+      following: false,
+      error: ""
     };
   }
 
@@ -24,6 +25,19 @@ class Profile extends Component {
       return follower._id === jwt.user._id;
     });
     return match;
+  };
+
+  clickFollowButton = (callApi) => {
+    const userId = isAuthenticated().user._id;
+    const token = isAuthenticated().token;
+
+    callApi(userId, token, this.state.user._id).then((data) => {
+      if (data.error) {
+        this.setState({ error: data.error });
+      } else {
+        this.setState({ user: data, following: !this.state.following });
+      }
+    });
   };
 
   init = (userId) => {
@@ -90,7 +104,10 @@ class Profile extends Component {
                 <DeleteUser userId={user._id} />
               </div>
             ) : (
-              <FollowProfileButton following={this.state.following} />
+              <FollowProfileButton
+                following={this.state.following}
+                onButtonClick={this.clickFollowButton}
+              />
             )}
           </div>
         </div>
