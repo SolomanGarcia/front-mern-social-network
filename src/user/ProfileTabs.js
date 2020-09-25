4ac0ad4356251ac54a1e04348abecdb8afb118e8
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-24 20:01:39 -0600

    display followers

--- src/user/ProfileTabs.js
+++ src/user/ProfileTabs.js
@@ -13,26 +13,60 @@ class ProfileTabs extends Component {
             <hr />
             {followers.map((person, i) => (
               <div key={i}>
-                <div className="row">
-                  <div>
-                    <Link to={`/user/${person._id}`}>
-                      <img
-                        className="float-left mr-2"
-                        height="30px"
-                        onError={(i) => (i.target.src = `${DefaultProfile}`)}
-                        src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
-                        alt={person.name}
-                      />
-                      <div>
-                        <h3>{person.name}</h3>
-                      </div>
-                    </Link>
-
-                    <p style={{ clear: "both" }}> {person.about}</p>
-                  </div>
+                <div>
+                  <Link to={`/user/${person._id}`}>
+                    <img
+                      style={{
+                        borderRadius: "50%",
+                        border: "1px solid #212529"
+                      }}
+                      className="float-left mr-2"
+                      height="30px"
+                      width="30px"
+                      onError={(i) => (i.target.src = `${DefaultProfile}`)}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                      alt={person.name}
+                    />
+                    <div>
+                      <p className="lead">{person.name}</p>
+                    </div>
+                  </Link>
                 </div>
               </div>
             ))}
+          </div>
+
+          <div className="col-md-4">
+            <h3 className="text-primary">Following</h3>
+            <hr />
+            {following.map((person, i) => (
+              <div key={i}>
+                <div>
+                  <Link to={`/user/${person._id}`}>
+                    <img
+                      style={{
+                        borderRadius: "50%",
+                        border: "1px solid #212529"
+                      }}
+                      className="float-left mr-2"
+                      height="30px"
+                      width="30px"
+                      onError={(i) => (i.target.src = `${DefaultProfile}`)}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                      alt={person.name}
+                    />
+                    <div>
+                      <p className="lead">{person.name}</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="col-md-4">
+            <h3 className="text-primary">Posts</h3>
+            <hr />
           </div>
         </div>
       </div>
