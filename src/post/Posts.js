ae235234bfaa7560efd8d2fab5d5afca1b78c821
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-26 20:55:57 -0600

    add loading

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -65,7 +65,9 @@ class Posts extends Component {
     const { posts } = this.state;
     return (
       <div className="container">
-        <h2 className="mt-5 mb-5">Recent Posts</h2>
+        <h2 className="mt-5 mb-5">
+          {!posts.length ? "Loading..." : "Recent Posts"}
+        </h2>
         {this.renderPosts(posts)}
       </div>
     );
