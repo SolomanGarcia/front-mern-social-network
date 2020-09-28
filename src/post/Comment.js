ddc2eeb62cfd7211b1da7b37e16a6a9932c12f6b
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-28 17:09:13 -0600

    work on removing comments

--- src/post/Comment.js
+++ src/post/Comment.js
@@ -6,32 +6,77 @@ import DefaultProfile from "../images/avatar.jpg";
 
 class Comment extends Component {
   state = {
-    text: ""
+    text: "",
+    error: ""
   };
 
   handleChange = (event) => {
+    this.setState({ error: "" });
     this.setState({ text: event.target.value });
+  };
+
+  isValid = () => {
+    const { text } = this.state;
+    if (!text.length > 0 || text.length > 150) {
+      this.setState({
+        error: "Comment should not be empty and less than 150 characters long"
+      });
+      return false;
+    }
+    return true;
   };
 
   addComment = (e) => {
     e.preventDefault();
+
+    if (!isAuthenticated()) {
+      this.setState({ error: "Please sign in to leave a comment" });
+      return false;
+    }
+
+    if (this.isValid()) {
+      const userId = isAuthenticated().user._id;
+      const token = isAuthenticated().token;
+      const postId = this.props.postId;
+
+      comment(userId, token, postId, { text: this.state.text }).then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          this.setState({ text: "" });
+          // dispatch fresh list of comments to parent (SinglePost)
+          this.props.updateComments(data.comments);
+        }
+      });
+    }
+  };
+
+  deleteComment = (comment) => {
     const userId = isAuthenticated().user._id;
     const token = isAuthenticated().token;
     const postId = this.props.postId;
 
-    comment(userId, token, postId, { text: this.state.text }).then((data) => {
+    uncomment(userId, token, postId, comment).then((data) => {
       if (data.error) {
         console.log(data.error);
       } else {
-        this.setState({ text: "" });
-        // dispatch fresh list of comments to parent (SinglePost)
         this.props.updateComments(data.comments);
       }
     });
   };
 
+  deleteConfirmed = (comment) => {
+    let answer = window.confirm(
+      "Are you sure you want to delete your comment?"
+    );
+    if (answer) {
+      this.deleteComment(comment);
+    }
+  };
+
   render() {
     const { comments } = this.props;
+    const { error } = this.state;
 
     return (
       <div>
@@ -49,6 +94,13 @@ class Comment extends Component {
             <button className="btn btn-raised btn-success mt-2">Post</button>
           </div>
         </form>
+
+        <div
+          className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}
+        >
+          {error}
+        </div>
 
         <div className="col-md-12">
           <h3 className="text-primary">{comments.length} Comments</h3>
@@ -80,6 +132,20 @@ class Comment extends Component {
                       {comment.postedBy.name}{" "}
                     </Link>
                     on {new Date(comment.created).toDateString()}
+                    <span>
+                      {isAuthenticated().user &&
+                        isAuthenticated().user._id === comment.postedBy._id && (
+                          <>
+                            <span
+                              onClick={() => this.deleteConfirmed(comment)}
+                              className="text-danger float-right mr-1"
+                              style={{ cursor: "pointer" }}
+                            >
+                              Remove
+                            </span>
+                          </>
+                        )}
+                    </span>
                   </p>
                 </div>
               </div>
