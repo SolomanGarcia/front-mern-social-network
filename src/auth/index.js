3713fcadd0f0d7ea8352f751d33266c38f17020d
Soloman Garcia <soloman.garcia@gmail.com>
2020-09-29 09:19:01 -0600

    refactor auth/index.js

--- src/auth/index.js
+++ src/auth/index.js
@@ -35,6 +35,13 @@ export const authenticate = (jwt, next) => {
   }
 };
 
+export const setName = (name, next) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("username", JSON.stringify(name));
+    next();
+  }
+};
+
 export const signout = (next) => {
   if (typeof window !== "undefined") localStorage.removeItem("jwt");
   next();
@@ -57,4 +64,54 @@ export const isAuthenticated = () => {
   } else {
     return false;
   }
+};
+
+export const forgotPassword = (email) => {
+  console.log("email: ", email);
+  return fetch(`${process.env.REACT_APP_API_URL}/forgot-password/`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email })
+  })
+    .then((response) => {
+      console.log("forgot password response: ", response);
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const resetPassword = (resetInfo) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/reset-password/`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(resetInfo)
+  })
+    .then((response) => {
+      console.log("forgot password response: ", response);
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const socialLogin = (user) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/social-login/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    // credentials: "include", // works only in the same origin
+    body: JSON.stringify(user)
+  })
+    .then((response) => {
+      console.log("signin response: ", response);
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
